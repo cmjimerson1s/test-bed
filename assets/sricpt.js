799ff46fd16299b9dcf24184071cc2c7a807b0fb
cmjimerson1s <cmjimerson1s@gmail.com>
2022-11-30 11:42:54 +0000

    Test bed

---
+++ assets/sricpt.js
@@ -0,0 +1,33 @@
+// let num1 = Math.floor(Math.random() * 25) + 1;
+// let num2 = Math.floor(Math.random() * 25) + 1;
+// let maths = (num1 / num2);
+
+
+// if (result = 0){
+//     do {
+//         let num1 = Math.floor(Math.random() * 25) + 1;
+//         let num2 = Math.floor(Math.random() * 25) + 1;
+//         let math2 = (num1 / num2);
+//     } 
+//     while (result = 0);
+//     document.getElementById('maths').innerText = "1";
+// } else {
+//     document.getElementById('maths').innerText = maths;
+// }
+
+// if (Number.isInteger(maths)) {
+//     document.getElementById('maths').innerText = maths;
+let num1 = 10
+let num2 = 2
+let holder = 1
+
+if (holder === 3) {
+    document.getElementById('maths').innerText = holder;
+
+} else {
+    do { 
+       (num1 + num2) 
+    }    
+    while(holder < 5);
+    document.getElementById('maths').innerText = num1;
+}
